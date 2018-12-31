0638e2c5f41a191bf38f9859a0d603cd656e862f
QinZelin <38138000+QZLin@users.noreply.github.com>
2018-12-31 18:45:10 +0800

    Create ListView_onItemClickListener.java

---
+++ ListView_onItemClickListener.java
@@ -0,0 +1,11 @@
+listview.setOnItemClickListener(
+                new OnItemClickListener() {
+                    //list点击事件
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                    }
+                }
+
+
+        );
